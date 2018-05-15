e145b789ef8237ea65cea4768d99c79b43b00e5d
yunastrian <37417623+yunastrian@users.noreply.github.com>
2018-05-16 00:24:59 +0700

    Delete main.js

--- GaIB/main.js
+++
@@ -1,8 +0,0 @@
-function button(event,x){ 
-    event.preventDefault() 
-    var j = document.getElementsByClassName("mainin").length; 
-    var i;
-    i = 2;
-    document.getElementById(i).style.display = "none";
-    document.getElementById(x).style.display = "inline"; 
-}
